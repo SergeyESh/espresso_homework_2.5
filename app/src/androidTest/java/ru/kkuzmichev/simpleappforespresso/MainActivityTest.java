352930ef7344ba9df6c6683e8a35af80d9482425
SergeyESh <sergeyesh1989@yandex.ru>
2023-09-04 19:03:49 +0300

    Задание 2 и доп. к ДЗ 2.6 Espresso выполнены

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -3,9 +3,12 @@ package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -13,32 +16,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
+import androidx.test.filters.LargeTest;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.security.PublicKey;
-
 @RunWith(AndroidJUnit4.class)
-public class EspressoTest {
+public class MainActivityTest {
+
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void testName() {
-        ViewInteraction mainText =onView(withId(R.id.text_home));
-        mainText.check(matches(withText("This is home fragment")));
-    }
-
-    @Test
-    public void testGalleryForRecyclerView() {
+    public void mainActivityTest() {
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Open navigation drawer"),
                         childAtPosition(
@@ -54,9 +55,19 @@ public class EspressoTest {
         navigationMenuItemView.check(matches(isDisplayed()));
         navigationMenuItemView.perform(click());
 
-        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
-        recyclerView.check(CustomViewAssertions.isRecyclerView());
-        recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(10)));
+        ViewInteraction element = onView(withText("5"));
+        element.check(matches(isDisplayed()));
+        element.check(matches(withText("5")));
+    }
+
+    @Before
+    public void registerIdlingResources() {                 //Подключаемся к "счетчику"
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.getIdlingResource());
+    }
+
+    @After
+    public void unregisterIdlingResources() {               //Отключаемся от "счетчика"
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.getIdlingResource());
     }
 
     private static Matcher<View> childAtPosition(
